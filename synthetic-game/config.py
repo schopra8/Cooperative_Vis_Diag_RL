c0f8ce9d9ccc50e244fce75d881f6aba8a2e1ab5
Abhijeet <abhimailme@gmail.com>
2018-02-23 15:19:32 -0800

    Added config and main files

---
+++ synthetic-game/config.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+class config():
+  
+## Parameters for Qbot
+    A = A()
+    Q = Q()
+    class Q():
+    #Vocabulary size for Q bot
+        num_actions = 3
+    # Possible guesses at the end
+        num_classes = 144
+    #epsilon for exploration
+        epsilon_start = 1
+        epsilon_end = 0.1
+        epsilon_step = 0.001
+## Parameters for Abot
+    class A():
+    #Vocabulary size for A bot
+        num_actions = 4
+    #epsilon for exploration
+        epsilon_start = 1
+        epsilon_end = 0.1
+        epsilon_step = 0.001
+
+##Training parameters
+    #need Q bot to guess image representation at every round?
+    guess_every_round = False
+    #Number of rounds of dialog before cutoff
+    max_dialog_rounds = 2
+##Output files
+    output_dir = "./results/"
+    data_dir = 
---
+++ synthetic-game/main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+from model import Dialog_Bots
+from config import config
+def main():
+	model=Dialog_Bots(config)
