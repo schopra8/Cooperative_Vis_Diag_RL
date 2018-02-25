Fix trajectories + returns computation

synthetic-game/model.py
@@ -28,7 +28,7 @@ class Dialog_Bots(object):
 		}
 		return feed_dict
 
-	def run_dialog(self,batch_size=self.config.batch_size, rounds_dialog = 2, synthetic=True):
+	def run_dialog(self, batch_size=self.config.batch_size, rounds_dialog=2, synthetic=True):
 		""" Runs dialog for specified number of rounds:
 				1) Q Bot asks question
 				2) A Bot answers question based on history 
@@ -42,15 +42,19 @@ class Dialog_Bots(object):
 		#First encode the caption and image in both bots
 		q_bot_states = self.Qbot.encode_captions(self.caption_placeholder)
 		a_bot_states  = self.Abot.encode_captions_images(self.caption_placeholder, self.image_placeholder)
-		trajectories = [] * batch_size
+		q_bot_trajectories = [] * batch_size
+		a_bot_trajectories = [] * batch_size
 		if synthetic:
 			a_bot_recent_facts = [(-1, -1)] * batch_size # Sentinels for A Bot Fact 0
 			q_bot_facts = [] 
 		else:
-			continue # TODO: Not Yet Implemented
+			pass # TODO: Not Yet Implemented
 		guesses = []
 		for _ in xrange(rounds_dialog):
 			questions = self.Qbot.get_questions(q_bot_states) # QBot generates questions (Q_t)
+			for i, q in enumerate(questions): # Append to trajectory 
+				q_bot_trajectories[i].append((q_bot_states[-1], q))
+
 			question_encodings = self.Abot.encode_questions(questions) # ABot encodes questions (Q_t)
 			a_bot_states = self.Abot.encode_state_histories(	# ABot encodes states (State, Y, C, Q_t, F_{t-1})
 				self.image_placeholder,
@@ -60,23 +64,20 @@ class Dialog_Bots(object):
 				a_bot_states
 			)
 			answers = self.Abot.decode_answers(a_bot_states) # ABot generates answers (A_t)
-
 			a_bot_recent_facts = self.Abot.encode_facts(question_encodings, answers) # ABot generates facts (F_t)
+			# TODO: How do we account for the first state (image, caption)? It doesn't yield an action.
+			for i, a in enumerate(answers): # Append to trajectory 
+				a_bot_trajectories[i].append((a_bot_states[-1], a))
+
 			q_bot_facts = self.Qbot.encode_facts(questions, answers) # QBot encodes facts (F_t)
 			q_bot_states = self.Qbot.encode_state_histories(q_bot_states, q_bot_facts) # QBot encode states
 			if self.config.guess_every_round:
 				guesses.append(self.Qbot.generate_image_representations(q_bot_states))
-			for i, q in enumerate(questions):
-				trajectories[i].append(q)
-				trajectories[i].append(answers[i])
+
 		if not self.config.guess_every_round:
 			guesses = self.Qbot.generate_image_representations(q_bot_states)
 
-		# TODO: Rewrite trajectories + after confirmation from others.
-		# There should actually be two sets of trajectories. One set for Q Bot and another for A Bot.
-		# The Trajectories should be in the form of [(state, action), ...]
-		# Note: They are currently of the form [(q, a), ...], which I believe is incorrect (Sahil)
-		return trajectories, guesses, q_bot_states, a_bot_states
+		return guesses, q_bot_trajectories, a_bot_trajectories
 
 	def get_minibatches(self, batch_size=20):
 		# TODO Implement batching of captions, images
@@ -85,16 +86,18 @@ class Dialog_Bots(object):
 	def get_returns(self, trajectories, guesses, answers, gamma):
 		""" Gets returns for a list of trajectories. 
 			+1 Reward if guess == answer
-			+0 Otherwise
+			-1 Otherwise
 		"""
 		# TODO: Confirm the reward structure
 		all_returns = []
 		for i, g in enumerate(guesses):
 			path_returns = [0] * len(trajectories[i])
 			if g == answers[i]:
-				# Correct answer, otherwise vector of all 0s
-				discount_factors = np.power(gamma, np.arange(len(trajectories[i])))
-				path_returns = list(np.multiply(np.ones(len(trajectories[i]), discount_factors)))
+				final_reward = 1.0
+			else:
+				final_reward = -1.0
+			discount_factors = np.power(gamma, np.arange(len(trajectories[i])))
+			path_returns = list(np.multiply(final_reward * np.ones(len(trajectories[i]), discount_factors)))
 			all_returns.append(path_returns)
 		return all_returns
 