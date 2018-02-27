1f09aed6ee910f27b53555d30e72648d7d705830
Abhijeet <abhimailme@gmail.com>
2018-02-27 01:32:40 -0800

    Bug fix in get_minibatch

--- synthetic-game/model.py
+++ synthetic-game/model.py
@@ -76,11 +76,7 @@ class Dialog_Bots(object):
 			batch_data = data[i%size:(i%size+batch_size),:]
 			images = batch_data[:,:3]
 			captions = batch_data [:,3]
-			#generate labels from data_file
-			labels = np.asarray([])
-			for i in images.shape[0]:
-				np.append(labels, images[i,caption_lookup[captions[i]]], axis=0)
-			i += batch_size
+			labels = batch_data[:,4]
 			yield zip(images, captions, labels)
 
 	def get_returns(self, trajectories, predictions, labels, gamma):
