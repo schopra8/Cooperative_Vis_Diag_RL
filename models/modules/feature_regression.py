e9edcfd6e444898452eccab9e1273c2b61b79672
Abhijeet <abhimailme@gmail.com>
2018-03-04 05:14:38 -0800

    Added feature regression network

--- models/modules/feature_regression.py
+++ models/modules/feature_regression.py
@@ -1,1 +1,30 @@
 class feature_regression():
+	def __init__(self, image_dimension, scope):
+		"""
+		Initialization function
+		====================
+		INPUTS:
+		image_dimension: int - shape of the image respresentation
+		====================
+		"""
+		self.image_dimension = image_dimension
+		self.scope = scope
+		self.add_cells()
+
+
+	def generate_image_prediction(self, state):
+		"""
+		Builds the graph to take in the question, answer and generates the new fact
+		===================================
+		INPUTS:
+		state: float of shape (batch_size, hidden_dimensio ) - The state-history at this round of dialog
+		===================================
+		OUTPUTS:
+		image_prediction: float of shape (batch_size, image_prediction) - The new image prediction based on dialog so far
+		"""
+		##Assumed that fully connected layer has no activation at output!
+
+		with tf.varible_scope(self.scope):
+			
+			image_prediction = tf.contrib.layers.fully_connected(state, self.image_dimension, activation_fn = None)
+			return image_prediction
