9a915fd057de777a2c1ac458152d1104cb7aaba6
schopra8 <schopra8@stanford.edu>
2018-03-05 15:48:59 -0800

    Finish concatenate q_a implementation

--- visdial-game/model.py
+++ visdial-game/model.py
@@ -29,17 +29,32 @@ class model():
 		pass
 
 	def evaluate(self, minibatch_generator, max_dialog_rounds):
-
+		pass
 
 	def show_dialog(self, image, caption, answer):
 		pass
 
-def concatenate_q_a(self, questions, question_lengths, answers, answer_lengths):
-	batch_size = tf.shape(questions)[0]
-	stripped_question_answer_pairs = [tf.concat([questions[i,0:question_lengths[i],:], answer[i,0:answer_lengths[i],:]], axis = 1)for i in xrange(batch_size)]
-	max_size = self.config.MAX_QUESTION_LENGTH + self.config.MAX_ANSWER_LENGTH
-	padded_question_answer_pairs = [tf.pad(stripped_question_answer_pairs[i], [0, max_size - tf.shape(stripped_question_answer_pairs[i])[0]]) for i in xrange(batch_size)]
-	question_answer_pairs = tf.stack(padded_question_answer_pairs, axis = 0)
+	def concatenate_q_a(self, questions, question_lengths, answers, answer_lengths):
+		"""
+		Concatenate question, answer pairs
+		===================================
+		INPUTS:
+		questions: float of shape (batch_size, max_question_length) -- tensor where each row are indices into vocabulary
+		question_lengths: int of shape (batch_size) -- tensor listing true length of each question in questions tensor
+		answers: float of shape (batch_size, max_answer_length) -- tensor where each row are indices into vocabulary
+		answer_lengths: int of shape (batch_size) -- tensor listing true length of each answer in answers tensor
+		===================================
+		OUTPUTS:
+		question_answer_pairs: float of shape (batch_size, max_question_length + max_answer_length): The sequence of output vectors for every timestep
+		question_answer_pair_lengths = (batch_size): The actual length of the question, answer concatenations
+		"""
+		batch_size = tf.shape(questions)[0]
+		stripped_question_answer_pairs = [tf.concat([questions[i,0:question_lengths[i],:], answer[i,0:answer_lengths[i],:]], axis = 1)for i in xrange(batch_size)]
+		max_size = self.config.MAX_QUESTION_LENGTH + self.config.MAX_ANSWER_LENGTH
+		padded_question_answer_pairs = [tf.pad(stripped_question_answer_pairs[i], [0, max_size - tf.shape(stripped_question_answer_pairs[i])[0]]) for i in xrange(batch_size)]
+		question_answer_pairs = tf.stack(padded_question_answer_pairs, axis = 0)
+		question_answer_pair_lengths = tf.add(question_lengths, answer_lengths)
+		return question_answer_pairs, question_answer_pair_lengths
 
 
 	
