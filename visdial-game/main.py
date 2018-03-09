821ff3cda06c5fbea12bfeb23e7e1aa1c963ab56
Ubuntu <schopra@cs234.0qp0o512auhezmufigwedjw34f.xx.internal.cloudapp.net>
2018-03-09 22:28:36 +0000

    Spacing issues

--- visdial-game/main.py
+++ visdial-game/main.py
@@ -6,11 +6,11 @@ def main():
 	with tf.Session() as sess:
 		ckpt = tf.train.get_checkpoint_state(self.config.model_save_directory)
     	v2_path = ckpt.model_checkpoint_path + ".index" if ckpt else ""
-    	if ckpt and (tf.gfile.Exists(ckpt.model_checkpoint_path) or tf.gfile.Exists(v2_path)):
+    	if (ckpt and (tf.gfile.Exists(ckpt.model_checkpoint_path)) or tf.gfile.Exists(v2_path)):
         	print ("Reading model parameters from %s" % ckpt.model_checkpoint_path)
         	visdial_bots.saver.restore(sess, ckpt.model_checkpoint_path)
-		else:
-			sess.run(tf.global_variables_initializer())
+	else:
+		sess.run(tf.global_variables_initializer())
 		visdial_bots.train(sess, num_epochs = config.num_epochs, batch_size = config.batch_size)
 
 
